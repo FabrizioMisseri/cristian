43a84e6add7f197c7ec4dfb8d1cd66c3c5a65e4e
FabrizioMisseri <misserifabrizio@gmail.com>
2023-08-12 19:22:27 +0200

    add: singleSocial-component

--- src/components/socialSection.vue
+++ src/components/socialSection.vue
@@ -1,12 +1,16 @@
 <script>
+import SingleSocial from '../mini-components/singleSocial.vue';
+import singleSocial from '../mini-components/singleSocial.vue';
+
 export default {
     name: "socialSection",
+    components: { SingleSocial }
 }
 </script>
 
 <template>
-    <section class="container">
-
+    <section class="container my-5 py-5">
+        <SingleSocial v-for="n in 3"></SingleSocial>
     </section>
 </template>
 
---
+++ src/mini-components/singleSocial.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: "singleSocial",
+}
+</script>
+
+<template>
+    <section class="row my-4">
+
+        <a href="#" class="btn btn-primary col-8 offset-2">
+            social X
+        </a>
+
+    </section>
+</template>
+
+<style lang="scss" scoped></style>
