a83c2a261553f7a4f1d8b400046d220903ccf943
FabrizioMisseri <misserifabrizio@gmail.com>
2023-08-14 10:00:49 +0200

    *

--- src/components/photoSection.vue
+++ src/components/photoSection.vue
@@ -12,7 +12,7 @@ export default {
     components: { singlePhoto, AOS },
 
     // AOS --------------------
-    mounted() {
+    created() {
         AOS.init();
     },
     // AOS --------------------
--- src/components/socialSection.vue
+++ src/components/socialSection.vue
@@ -11,7 +11,7 @@ export default {
     components: { singleSocial, AOS },
 
     // AOS --------------------
-    mounted() {
+    created() {
         AOS.init();
     },
     // AOS --------------------
